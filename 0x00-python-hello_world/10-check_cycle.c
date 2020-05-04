861dc8818873623dcdc46b60c0a29422ace0025f
SimpleSteven <1648@holbertonschool.com>
2020-05-03 23:25:51 -0500

    Program to check if a linked list has a loop

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * check_cycle - check if the linked list has a loop
+  * @head: the head node of the linked list
+  * Return: return 1 if it has loop and 0 if it doesn't
+  **/
+
+int check_cycle(listint_t *head)
+{
+	listint_t *fast_node = head;
+	listint_t *slow_node = head;
+
+	while (fast_node->next->next && fast_node->next)
+	{
+		fast_node = fast_node->next->next;
+		slow_node = slow_node->next;
+		if (fast_node == slow_node)
+			return (1);
+	}
+	return (0);
+}

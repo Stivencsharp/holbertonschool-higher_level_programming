42b87d947eaa98e8ba8e3a788af0a620b465cb2b
SimpleSteven <1648@holbertonschool.com>
2020-05-05 11:56:38 -0500

    Second if fixed. Changed to a elif. Sentence "The last digit changed to "Last digit of".

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     last_digit = number % 10
 if last_digit > 5:
     is_than = "and is greater than 5"
-if last_digit is 0:
+elif last_digit is 0:
     is_than = "and is 0"
 else:
     is_than = "and is less than 6 and no 0"
-print("The last digit of " + str(number) + " is " + str(last_digit), is_than)
+print("Last digit of " + str(number) + " is " + str(last_digit), is_than)

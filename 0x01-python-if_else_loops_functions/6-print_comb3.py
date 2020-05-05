1d90d567cf08739f44c33cb1184faf5c7a9aed19
SimpleSteven <1648@holbertonschool.com>
2020-05-05 14:18:56 -0500

    Break line is last print added. Double line at the end of the file change to 1 line.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-numbers = "{}{}, "
-for first_digit in range(0, 9):
+for first_digit in range(0, 8):
     second_digit = 1 + first_digit
     for second_digit in range(second_digit, 10):
-        if first_digit is 8 and second_digit is 9:
-            numbers = "{}{}"
-        print(numbers.format(first_digit, second_digit), end="")
-
+        print("{}{}, ".format(first_digit, second_digit), end="")
+print(89)

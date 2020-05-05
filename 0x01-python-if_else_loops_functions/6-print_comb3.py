9765b7aae3b12f4107b136f98a89434cfe7ffa7b
SimpleSteven <1648@holbertonschool.com>
2020-05-05 14:13:34 -0500

    Print the numbers form 01 to 89 without repeat numbers. The first number: 01. Then 10 wouldn't be printed.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+numbers = "{}{}, "
+for first_digit in range(0, 9):
+    second_digit = 1 + first_digit
+    for second_digit in range(second_digit, 10):
+        if first_digit is 8 and second_digit is 9:
+            numbers = "{}{}"
+        print(numbers.format(first_digit, second_digit), end="")
+

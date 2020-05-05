f15cd61979c3e837c2b37c5e912e97c720276b06
SimpleSteven <1648@holbertonschool.com>
2020-05-05 14:49:29 -0500

    Change a string characters from lower to upper case.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for letter in str:
+        if 96 < ord(letter) < 123:
+            new_str += chr(ord(letter) - 32)
+        else:
+            new_str += letter
+    print(new_str)

a449c84b6380c0868835595c7afafa8be20ff314
SimpleSteven <1648@holbertonschool.com>
2020-05-12 23:31:36 -0500

    Just 1 argument case print: "1 argument:" and the argument.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
 arguments_len = len(sys.argv)
 if arguments_len < 2:
     print("0 arguments.")
+elif arguments_len == 2:
+    print("1 argument:".format(arguments_len - 1))
+    print("1: {:s}".format(sys.argv[1]))
 else:
     print("{:d} arguments:".format(arguments_len - 1))
     for arg in range(1, arguments_len):

8789ae668ec73d0f7dbd4001d934bbd69b0ea9da
SimpleSteven <1648@holbertonschool.com>
2020-05-12 22:26:51 -0500

    __import__ function used.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-import add_0
+_sum = __import__('add_0').add
 a, b = 1, 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(1, 2)))
+print("{:d} + {:d} = {:d}".format(a, b, _sum(1, 2)))

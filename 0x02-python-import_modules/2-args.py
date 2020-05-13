199d99a4dde4eb78b2b92de69ba19e0ad70f68a3
SimpleSteven <1648@holbertonschool.com>
2020-05-12 23:26:01 -0500

    Don't show the number of the program.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ arguments_len = len(sys.argv)
 if arguments_len < 2:
     print("0 arguments.")
 else:
-    print("{:d} arguments:".format(arguments_len))
+    print("{:d} arguments:".format(arguments_len - 1))
     for arg in range(1, arguments_len):
         print("{:d}: {:s}".format(arg, sys.argv[arg]))

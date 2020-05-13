09d03fdfdf8623121ef3b40efdbf8afe78fead61
SimpleSteven <1648@holbertonschool.com>
2020-05-13 00:06:31 -0500

    Write a basic calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+num_args = len(sys.argv)
+if num_args != 4:
+    print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
+    exit(1)
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[3])
+sign = sys.argv[2]
+if sign == '+':
+    print("{:d} {:s} {:d} = {:d}".format(num1, sign, num2, add(num1, num2)))
+elif sign == '-':
+    print("{:d} {:s} {:d} = {:d}".format(num1, sign, num2, sub(num1, num2)))
+elif sign == '*':
+    print("{:d} {:s} {:d} = {:d}".format(num1, sign, num2, mul(num1, num2)))
+elif sign == '/':
+    print("{:d} {:s} {:d} = {:d}".format(num1, sign, num2, div(num1, num2)))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")

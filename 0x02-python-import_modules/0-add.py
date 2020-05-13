7ffd906ec0b9ad61c541d5209b16d5ab4e86adc2
SimpleSteven <1648@holbertonschool.com>
2020-05-12 22:43:56 -0500

    "__import__" function change to "from add_0 import add".

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    _sum = __import__('add_0').add
+    from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, _sum(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

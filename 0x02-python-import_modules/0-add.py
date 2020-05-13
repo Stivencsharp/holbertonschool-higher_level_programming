13e084b2df0f22a80bc649170fa39420e1d5dd7b
SimpleSteven <1648@holbertonschool.com>
2020-05-12 22:33:23 -0500

    Pass a and b variables to the function call.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,4 @@
 if __name__ == "__main__":
     _sum = __import__('add_0').add
 a, b = 1, 2
-print("{:d} + {:d} = {:d}".format(a, b, _sum(1, 2)))
+print("{:d} + {:d} = {:d}".format(a, b, _sum(a, b)))

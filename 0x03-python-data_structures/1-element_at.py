58f5ff62cc22c91d8b7d7bf2e63670e9713242ca
SimpleSteven <1648@holbertonschool.com>
2020-05-22 10:53:32 -0500

    Return the index element of a list.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,8 +4,3 @@ def element_at(my_list, idx):
         pass
     else:
         return my_list[idx]
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))

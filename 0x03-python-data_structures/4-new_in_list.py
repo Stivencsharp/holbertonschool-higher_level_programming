04aa70b0d5b3932ac3061b7731824f0840d529fb
SimpleSteven <1648@holbertonschool.com>
2020-05-22 11:32:20 -0500

    Error that change the old list instead the new list changed.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if 0 > idx or idx > len(my_list) - 1:
         return new_list
-    my_list.pop(idx)
-    my_list.insert(idx, element)
+    new_list.pop(idx)
+    new_list.insert(idx, element)
     return new_list

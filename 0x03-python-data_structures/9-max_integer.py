0d3b1744ad466b08d11e88500f2a326421831dce
SimpleSteven <1648@holbertonschool.com>
2020-05-22 14:13:15 -0500

    Find the biggest digit in a list without the function max().

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    biggest_num = my_list[0]
     for num in my_list:
-        if num % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+        if biggest_num < num:
+            biggest_num = num
+    return biggest_num

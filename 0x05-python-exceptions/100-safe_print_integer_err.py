e99e01a5f5241d684e446e1a45645eea3ffaf72b
SimpleSteven <1648@holbertonschool.com>
2020-06-03 00:01:13 -0500

    Show the error in stderr with a break line and the word "Exception" at the beggining.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -12,19 +12,3 @@ def safe_print_integer_err(value):
         sys.stderr.write("Exception: " + str(err) + "\n")
         return False
     return True
-
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "Holberton"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))

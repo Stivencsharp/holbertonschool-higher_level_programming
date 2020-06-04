371ed06bd446ce07ad031d06dcfbf8eba0cba211
SimpleSteven <1648@holbertonschool.com>
2020-06-04 17:20:06 -0500

    Create a class with a position, size, area and a print function based on the size and the position to calculate the coordinates of the square.

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Creating a Square class"""
+
+
+class Square:
+    """A Square class with a size as instance variable"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        for item in position:
+            if not isinstance(item, int):
+                raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            print(self.position[1] * '\n', end='')
+            for column in range(self.size):
+                print("{}{}".format(self.position[0] * ' ', self.size * '#'), end='')
+                print()

cb2110c89a83570b6efca944eab486998f3b2e0b
SimpleSteven <1648@holbertonschool.com>
2020-06-04 16:40:10 -0500

    Create a class with two instance variables, size that is private and area that is public. And a instance method to print a square depending on the size.

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Creating a Square class"""
+
+
+class Square:
+    """A Square class with a size as instance variable"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for column in range(self.size):
+                for row in range(self.size):
+                    print("#", end='')
+                print()
+
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")

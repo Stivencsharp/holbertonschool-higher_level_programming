8eeba23ca3c27e18567414d33df60e6ec1014c58
SimpleSteven <1648@holbertonschool.com>
2020-06-05 12:08:57 -0500

    Add the public instance methods: Area (weight * width) and Perimeter ((weight + width) * 2).

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Create a Rectangle Class"""
+
+
+class Rectangle:
+    """Rectangle Class with two attrbutes. Width and Height"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return (self.height + self.width) * 2

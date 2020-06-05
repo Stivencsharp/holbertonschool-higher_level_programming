141efcca41bff127e598aa4eea4affdeb47b9be4
SimpleSteven <1648@holbertonschool.com>
2020-06-04 19:40:01 -0500

    Almost complete.

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -50,18 +50,3 @@ class SinglyLinkedList:
             current_node = current_node.next_node
         new_node.next_node = current_node.next_node
         current_node.next_node = new_node
-
-
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)

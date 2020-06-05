15e1dc29b94b8caa749f801c3f330311f052ff63
SimpleSteven <1648@holbertonschool.com>
2020-06-04 19:37:56 -0500

    Linked list in python (Incomplete).

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Creating a Node class"""
+
+
+class Node:
+    """A node class for a linked list"""
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        if not isinstance(data, int):
+            raise TypeError("data must be an integer")
+        self.__data = data
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """The linked list class"""
+
+    def __init__(self):
+        self.head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        if current_node.data > new_node.data:
+            new_node.next_node = current_node
+            self.head = new_node
+            return
+        while current_node.next_node is not None and \
+                new_node.data > current_node.next_node.data:
+            current_node = current_node.next_node
+        new_node.next_node = current_node.next_node
+        current_node.next_node = new_node
+
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)

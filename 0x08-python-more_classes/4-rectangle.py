5b6a3951ab73d20c09a776c570fcdec9e1b94d4b
SimpleSteven <1648@holbertonschool.com>
2020-06-05 16:01:11 -0500

    Return a string representation of the rectangle to passed to the eval later. In the __repr__ function.

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Create a Rectangle Class"""
+
+
+class Rectangle:
+    """Rectangle Class with two attrbutes. Width and Height"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.height == 0 or 0 == self.width:
+            return 0
+        return (self.height + self.width) * 2
+
+    def __str__(self):
+        if self.height == 0 or 0 == self.width:
+            return ""
+        rect_str = ""
+        _rectangle = []
+        for column in range(self.height):
+            if column == self.height - 1:
+                _rectangle.append(self.width * '#')
+            else:
+                _rectangle.append(self.width * '#' + '\n')
+            rect_str += _rectangle[column]
+        return rect_str
+
+    def __repr__(self):
+        return f'Rectangle({self.width}, {self.height})'

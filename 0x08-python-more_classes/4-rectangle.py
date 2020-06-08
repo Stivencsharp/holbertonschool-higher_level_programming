da74a2bdf6d6743b3ee7122da0715abe2e376af7
SimpleSteven <1648@holbertonschool.com>
2020-06-08 14:09:26 -0500

    Everything is documented and has a new line after the doc.

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -5,21 +5,24 @@
 
 
 class Rectangle:
-    
-    
     """ Rectangle Class with two attrbutes. Width and Height """
-    
 
     def __init__(self, width=0, height=0):
+        """The init function with the width and height as arguments"""
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """The getter function of the width attribute"""
+
         return self.__width
 
     @width.setter
     def width(self, width):
+        """The setter function of the width attribute"""
+
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
@@ -28,10 +31,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """The getter function of the height attribute"""
+
         return self.__height
 
     @height.setter
     def height(self, height):
+        """The setter function of the height attribute"""
+
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
@@ -39,14 +46,21 @@ class Rectangle:
         self.__height = height
 
     def area(self):
+        """The getter and setter function of the area attribute"""
+
         return self.height * self.width
 
     def perimeter(self):
+        """The getter and setter function of the perimeter attribute"""
+
         if self.height == 0 or 0 == self.width:
             return 0
         return (self.height + self.width) * 2
 
     def __str__(self):
+        """The str function to return a string with the rectangle in '#' based
+        on the height and width"""
+
         if self.height == 0 or 0 == self.width:
             return ""
         rect_str = ""
@@ -60,4 +74,7 @@ class Rectangle:
         return rect_str
 
     def __repr__(self):
+        """The repr function to return a string representation of the
+        rectngle to recreate a new instance later"""
+
         return f'Rectangle({self.width}, {self.height})'

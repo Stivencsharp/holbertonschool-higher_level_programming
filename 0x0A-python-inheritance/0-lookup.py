e02450513cec421b7145854daf493f4139b1ae53
SimpleSteven <1648@holbertonschool.com>
2020-06-12 16:43:21 -0500

    Change the 'dir([obj])' to 'dir(obj)'.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-'''Function with a class as argument
+'''Function with an object as argument
 that return the result of the function dir()'''
 
 
 def lookup(obj):
     '''Function to return the attributes and methods
-    available in a class'''
+    available in an object'''
 
-    return dir([obj])
+    return dir(obj)
+
+
+class MyClass1(object):
+    pass

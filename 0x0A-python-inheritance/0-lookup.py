d74546e57a5c1512bc20ad5e775845d2340d8b13
SimpleSteven <1648@holbertonschool.com>
2020-06-12 16:25:53 -0500

    Docstring added.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+'''Function with a class as argument
+that return the result of the function dir()'''
+
+
 def lookup(obj):
+    '''Function to return the attributes and methods
+    available in a class'''
+
     return dir([obj])

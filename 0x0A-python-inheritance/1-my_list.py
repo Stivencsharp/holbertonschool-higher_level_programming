94a6254461950c9858da2652f1daa22d1e4064cd
SimpleSteven <1648@holbertonschool.com>
2020-06-12 17:36:32 -0500

    Create a class that has a list with the function, append and print_sorted(to print the list sorted).

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Create a class MyList that inherits from list'''
+
+
+class MyList(list):
+    '''Class MyList that inherits from list'''
+
+    def __init__(self):
+        self._list = []
+
+    def print_sorted(self):
+        print(sorted(self._list))
+
+    def append(self, value):
+        self._list.append(value)
+
+    def __str__(self):
+        print(self._list, end='')
+        return ''

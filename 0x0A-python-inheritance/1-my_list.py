cb13b108241919d42281980cd9dc8beb1826480a
SimpleSteven <1648@holbertonschool.com>
2020-06-13 22:36:24 -0500

    Just the printed_sorted() function stay. All the functions deleted.

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,19 +5,5 @@
 class MyList(list):
     '''Class MyList that inherits from list'''
 
-    def __init__(self):
-        self._list = []
-
     def print_sorted(self):
         print(sorted(self._list))
-
-    def append(self, value):
-        self._list.append(value)
-
-    def __str__(self):
-        print(self._list, end='')
-        return ''
-
-
-my_list = MyList()
-my_list.print_sorted()

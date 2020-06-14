4d2491d427025aefc0b0595826e409cab893e7d2
SimpleSteven <1648@holbertonschool.com>
2020-06-13 23:20:49 -0500

    Add the documentation.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+'''Holberton task about instances of classes'''
+
+
 def is_same_class(obj, a_class):
+    '''Check if the object is instance of the class
+    Return True if it is, if not, return False'''
+
     return type(obj) is a_class

8f04f2282b5deaa340624d02958f4340aa78db55
SimpleSteven <1648@holbertonschool.com>
2020-06-13 22:30:21 -0500

    Return a new list in print_sorted() function.

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,7 +9,7 @@ class MyList(list):
         self._list = []
 
     def print_sorted(self):
-        print(sorted(self._list))
+        return sorted(self._list)
 
     def append(self, value):
         self._list.append(value)

ff8061028e9a18d5a70afd8f176680f6a6a40885
SimpleSteven <1648@holbertonschool.com>
2020-06-23 18:02:22 -0500

    Docstring added.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+'''Return if a obj inherits from a class'''
+
+
 def inherits_from(obj, a_class):
+    '''Return True is a object inherits from a class. Otherwise, return False.'''
+
     return a_class is not type(obj) and issubclass(type(obj), a_class)

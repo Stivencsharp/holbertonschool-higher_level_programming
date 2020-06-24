e47d4fb5ee0f25a4e0478615558548f2796e274f
SimpleSteven <1648@holbertonschool.com>
2020-06-24 08:38:46 -0500

    Add instance method area() to the BaseGeometry class that raise an exception.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Create a class named BaseGeometry'''
+
+
+class BaseGeometry:
+    '''A class BaseGeometry with a instance method area()'''
+
+    def area(self):
+        raise Exception("area() is not implemented")

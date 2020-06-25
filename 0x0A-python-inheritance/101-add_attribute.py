12ee9d4a07dd7618bc5b552ba3015db88b9b7674
SimpleSteven <1648@holbertonschool.com>
2020-06-25 13:36:20 -0500

    Add docstring

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+'''Create a function to add a new attribute to a class'''
+
+
 def add_attribute(a_class, name, value):
+    '''Create a function that add a new attribute to a class. If fail,
+    raise an error with a customized message.'''
+
     if not (setattr(a_class, name, value)):
         raise TypeError("can't add new attribute")

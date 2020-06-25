247bb0502bb75eabef30f389c474eb16c7762120
SimpleSteven <1648@holbertonschool.com>
2020-06-25 11:51:24 -0500

    Import the class from the script 7 instead of writing it again.

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,18 +2,7 @@
 '''Create a class named BaseGeometry'''
 
 
-class BaseGeometry:
-    '''A class BaseGeometry with a instance method area()'''
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        elif value < 1:
-            raise ValueError("{:s} must be greater than 0".format(name))
-        return value
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

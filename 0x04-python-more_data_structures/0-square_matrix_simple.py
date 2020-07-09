fac595d79a06dc0a146fa9c6f4245fa7dbb5bb55
SimpleSteven <1648@holbertonschool.com>
2020-07-08 21:31:08 -0500

    Fixed the just "original item by 2" to "original item squared".

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[item*2 for item in sublist] for sublist in matrix]
+    new_matrix = [[item**2 for item in sublist] for sublist in matrix]
     return new_matrix
